c51064b0d239dd89776cc02046f934ef612437f1
harshilsaraiya <64249107+harshilsaraiya@users.noreply.github.com>
2021-05-28 21:09:06 +0530

    Add files via upload

---
+++ Day2/Day2(1).php
@@ -0,0 +1,12 @@
+<?php
+    echo "welcome to php";
+    echo "<h1> I am H1 in PHP </h1>";
+    ?>
+    <html>
+         <body>
+            welcome to html code <br>
+              <?php
+                 echo "I am php inside HTML";
+                 ?>
+         </body>
+    </html>
---
+++ Day2/Day2(3).php
@@ -0,0 +1,14 @@
+<?php 
+   
+    $a = 3;
+     switch($a) {
+         case 1: echo "One";
+                 break;
+         case 2: echo "Two";
+                 break; 
+         case 3: echo "Three";
+                 break; 
+                 
+            default:
+                   echo "please select from 1-3";
+     }
